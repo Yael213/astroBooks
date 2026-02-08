f782fca6aadef0b8e7c8e1836c391b2477ab437b
Yael213 <yaelgalvanmata@gmail.com>
2026-02-08 13:28:07 -0600

    Segundo commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import Input_ from 'postcss/lib/input';
 
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
@@ -21,7 +23,5 @@ export default defineConfig({
     }
   },
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
